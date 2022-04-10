836b1803685d2fe19ed7169119afc9f1d8a610ac
Konstantin Tugolukov <031184@bk.ru>
2022-04-10 13:18:56 +0300

    Initial Commit

---
+++ AdvTeamTestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AdvTeamTestProject
+//
+//  Created by Константин Туголуков on 10.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
